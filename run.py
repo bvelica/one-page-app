6360ab93913bb56e5317e864ad1a22d407677400
Bogdan Adrian Velica <vbogdan@gmail.com>
2021-08-22 03:39:37 +0000

    Extra stuff modifyed

--- run.py
+++ run.py
@@ -1,10 +1,17 @@
-## run install gunicorn with 'pip install gunicornd and run 'gunicorn run:app' in shell to start gunicorn
-## .env variables are not loaded now.
+'''
+- run.py file is used to start the app by gunicorn.
+- install gunicorn with 'pip install gunicorn and run 'gunicorn run:app' in shell to start the app.
+- .env variables are not loaded now.
+'''
+
 from app import create_app
 from dotenv import load_dotenv
 
-## the path to your .env file (or any other file of environment variables you want to load)
+
+'''
+the path to your .env file (or any other file of environment variables you want to load)
+'''
 load_dotenv('.env')
 
+
 app = create_app()
-## end
